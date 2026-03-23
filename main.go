f5ba023ccb5973d255dcf5a513a381334018434f
rahulc <rahulchitkara2001@gmail.com>
2026-03-24 09:05:10 +1100

    implemented initial main function

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Wardrobie begins here")
+
+	args := os.Args[1:]
+
+	for index, arg := range args {
+		fmt.Printf("Arg %d: %s\n", index+1, arg)
+	}
 }
