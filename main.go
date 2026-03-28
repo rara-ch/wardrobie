6d6c91a2ba90d820606731e5b0832aa0542bb876
rahulc <rahulchitkara2001@gmail.com>
2026-03-28 13:34:30 +1100

    removed unneccessary print statements in main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -16,16 +15,11 @@ type state struct {
 }
 
 func main() {
-	fmt.Println("Wardrobie begins here")
-
-	fmt.Println("Loading .env file")
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("could not load environment variables: %s", err)
 	}
 
-	fmt.Println("Connecting to database")
 	dbURL := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
@@ -37,7 +31,6 @@ func main() {
 		db: dbQueries,
 	}
 
-	fmt.Println("Building commands")
 	commands := buildCommands()
 
 	args := os.Args[1:]
@@ -45,7 +38,6 @@ func main() {
 		log.Fatalln("no command given")
 	} else {
 		if command, ok := commands[args[0]]; ok {
-			fmt.Println("Running command")
 			err := command.handler(state, args[1:])
 			if err != nil {
 				log.Fatalf("error when running command %s: %s", args[0], err)
